675257d7a64bc276c523ccc2d3943dac29607361
zidvsd <rashidvisda@gmail.com>
2025-02-04 19:27:40 +0800

    dsa

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ menuBtn.addEventListener("click", () => {
   console.log("dasd");
   menuOverlay.classList.toggle("open");
 });
+
+const audioPlayer = document.getElementById("audioPlayer");
+
+// Set volume to a lower value when the audio starts playing
+audioPlayer.volume = 0.3;
